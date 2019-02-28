93b1527f8dcd1f35551e41b8667b9ca969388cbf
김병곤 <qudrhs190@gmail.com>
2019-02-28 09:56:03 +0900

    ADD OEPNING_IMDF_FILE.h

--- TRANSFORM/IMDF_FILE.h
+++ TRANSFORM/IMDF_FILE.h
@@ -50,81 +50,47 @@ namespace IMDF{
             std::vector<feature*>features;
         };
     }
-//    namespace address {
-//        class propertires {
-//        public:
-//            std::string address;
-//            std::string unit;
-//            std::string locality;
-//            std::string province;
-//            std::string country;
-//            std::string postal_code;
-//            std::string code_ext;
-//            std::string postal_code_vanity;
-//        };
-//
-//        class feature {
-//        public:
-//            std::string type;
-//            std::string id;
-//            std::string feature_type;
-//            std::string geometry;
-//
-//            class propertires propertires;
-//        };
-//        class features {
-//        public:
-//            std::vector<feature*> features;
-//        };
-//        class address{
-//        public:
-//            std::string type;
-//            std::string name;
-//            features *features;
-//        };
-//    }
-//
-//    namespace amenity{
-//        class properties{
-//            std::string category;
-//            std::string accesibility;
-//            std::string name;
-//            std::string alt_name;
-//            std::string hours;
-//            std::string phone;
-//            std::string website;
-//            std::vector<std::string>ids;
-//            std::string address_id;
-//            std::string correlation_id;
-//        };
-//        class point{
-//        public:
-//            double pointx;
-//            double pointy;
-//        };
-//        class coordinates{
-//        public:
-//            std::vector<point*> point;
-//        };
-//        class geometry{
-//        public:
-//            std::string type;
-//            coordinates * coordi;
-//        };
-//        class feature{
-//            std::string type;
-//            std::string id;
-//            std::string feature_type;
-//            geometry geometry;
-//            properties *properties;
-//        };
-//        class amenity{
-//        public:
-//            std::string type;
-//            std::string name;
-//            std::vector <feature*>features;
-//        };
-//    }
-
+    namespace OPENING{
+        class point{
+        public:
+            double pointx;
+            double pointy;
+        };
+        class display_point{
+        public:
+            std::string type;
+            std::vector<point*> display_points;
+        };
+        class geometry{
+        public:
+            std::string type;
+            std::vector<point*> coordinates;
+        };
+        class properties{
+        public:
+            std::string category;
+            std::string accessibility;
+            std::string access_control;
+            std::string door;
+            std::string name;
+            std::string alt_name;
+            display_point *properties_display_point;
+            std::string level_id;
+        };
+        class feature{
+        public:
+            std::string type;
+            std::string id;
+            std::string feature_type;
+            geometry *feature_geometry;
+            properties *feature_properties;
+        };
+        class opening{
+        public:
+            std::string type;
+            std::string name;
+            std::vector<feature*>features;
+        };
+    }
 }
 #endif //IN2OSM_IMDF_FILE_H
