a6554e735d6ff12492d42d37e6cf63510e85e2d8
김병곤 <qudrhs190@gmail.com>
2019-02-28 09:55:24 +0900

    ADD IMDF

--- CONVERTER.cpp
+++ CONVERTER.cpp
@@ -11,17 +11,45 @@
 #include <string.h>
 #include <io.h>
 #include "assert.h"
+
+const char *DATA[3] = {"indoorgml", "osm", "imdf"};
+
 int main(int argc, char **argv) {
-    try{
-        if(!IO::ISNOT_INDOORGML(argv))
+    std::vector<CONVERTER::IC *> IC_vector_MEMORY;
+    int input;
+    int output;
+    try {
+        if (!IO::ISNOT_INDOORGML(argv))
             throw argv;
-        if(strcmp(IO::lowercase(argv[1]),"indoorgml")==0 && strcmp(IO::lowercase(argv[2]),"osm")==0){
-            OSM::Write(INDOOR::Read(argv[3]),argv[4]);
+
+        for (int i = 0; i < 3; i++) {
+            if (strcmp(IO::lowercase(argv[1]), DATA[i]) == 0) {
+                input = i;
+            }
+            if (strcmp(IO::lowercase(argv[2]), DATA[i]) == 0) {
+                output = i;
+            }
         }
-        if(strcmp(IO::lowercase(argv[1]),"osm")==0 && strcmp(IO::lowercase(argv[2]),"indoorgml")==0){
-            INDOOR::Write(OSM::Read(argv[3]),argv[4]);
+        switch (input) {
+            case 0:
+                IC_vector_MEMORY = INDOOR::Read(argv[3]);
+                break;
+            case 1:
+                IC_vector_MEMORY = OSM::Read(argv[3]);
+                break;
+            case 2:
+                IC_vector_MEMORY = IMDF::READ(argv[3]);
+                break;
         }
-    } catch(char **argv) {
+        switch (output) {
+            case 0:
+                INDOOR::Write(IC_vector_MEMORY, argv[4]);
+                break;
+            case 1:
+                OSM::Write(IC_vector_MEMORY, argv[4]);
+                break;
+        }
+    } catch (char **argv) {
         IO::ISNOT_INDORGML_MESSAGE();
     }
     return 0;
